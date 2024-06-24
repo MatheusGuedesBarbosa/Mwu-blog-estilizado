3cba5578df86de796d79c7e539d16be6e1235f7c
MatheusGuedesBarbosa <matheus.guedes.barbosa22022003@gmail.com>
2024-06-24 10:36:13 -0300

    Meu primeiro blog estilizado

---
+++ javascript.js
@@ -0,0 +1,17 @@
+/*function Enviar(){
+    let nome = document.getElementById("txtNome")
+    let email = document.getElementById("txtE-mail")
+    let Mensagem = document.getElementById("txtMensagens")
+
+    if( nome.value == '' &&  email.value == '' && Mensagem.value == '')
+    {
+        alert(`Preencham os campos vazios`)
+    }
+    else
+    {
+        alert(`Mensagens enviada com sucesso!`)
+        '<form class="Fale-comigo" action="https://formsubmit.co/matheus.gbarbosa22022003@gmail.com" method="POST">'
+    
+    }
+ 
+}*/
